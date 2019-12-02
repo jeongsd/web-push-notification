36269dbcbf32e7345b35652abf697263b7352439
jeongsd <asdfg852456@gmail.com>
2019-12-02 21:47:33 +0900

    class -> function component

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,15 @@ import React from "react";
 import logo from "./cloud.svg";
 import "./Home.css";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className='Home'>
-        <div className='Home-header'>
-          <img src={logo} className='Home-logo' alt='logo' />
-          <h2>Web Push Notification</h2>
-        </div>
+const Home = () => {
+  return (
+    <div className='Home'>
+      <div className='Home-header'>
+        <img src={logo} className='Home-logo' alt='logo' />
+        <h2>Web Push Notification</h2>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
